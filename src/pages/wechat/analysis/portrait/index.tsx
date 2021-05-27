f4b4969914161c126db2f3244a2af725e46455a3
88fantasy <88fantasy@gmail.com>
2021-05-27 17:43:29 +0800

    portralt update

--- src/pages/wechat/analysis/portrait/index.tsx
+++ src/pages/wechat/analysis/portrait/index.tsx
@@ -3,7 +3,7 @@ import ProCard, { StatisticCard } from '@ant-design/pro-card';
 import { listUserPortrait } from '../service';
 import type { UserPortraitItem, UserPortraitResponse } from '../data';
 import moment from 'moment';
-import { DatePicker,  Space, Select, Typography } from 'antd';
+import { DatePicker, Space, Select, Typography } from 'antd';
 import { Pie, Bar } from '@ant-design/charts';
 
 const Index: React.FC<{ appId: string }> = (props) => {
@@ -21,7 +21,6 @@ const Index: React.FC<{ appId: string }> = (props) => {
         .then((res) => {
           if (res && res.status && !res.data.errcode) {
             setDataSource(res.data);
-            res.data.visitUv.platforms.filter((v) => v.value > 0);
           }
         })
         .finally(() => {
@@ -29,6 +28,11 @@ const Index: React.FC<{ appId: string }> = (props) => {
         });
     }
   }, [appId, date, range]);
+
+  const total = dataSource ? dataSource[user].platforms.reduce((_total: number, current: UserPortraitItem) => _total + current.value, 0,) : 0;
+
+  const manItem: UserPortraitItem = dataSource ? dataSource[user].genders.find((v: { name: string; }) => v.name === '男') : { value: 0};
+  const womanItem: UserPortraitItem = dataSource ? dataSource[user].genders.find((v: { name: string; }) => v.name === '女') : { value: 0};
 
   return (
     <ProCard
@@ -65,8 +69,7 @@ const Index: React.FC<{ appId: string }> = (props) => {
             <Select.Option value="visitUvNew">新增用户</Select.Option>
           </Select>
 
-
-          <Typography.Text>{dataSource? dataSource.refDate : ''}</Typography.Text>
+          <Typography.Text>{dataSource ? dataSource.refDate : ''}</Typography.Text>
         </Space>
       }
       extra={<a href="#">下载</a>}
@@ -75,9 +78,67 @@ const Index: React.FC<{ appId: string }> = (props) => {
       bordered
       loading={loading}
     >
-      <ProCard split="horizontal">
+      <ProCard split="horizontal" colSpan="70%">
         <ProCard split="horizontal">
-          <ProCard split="vertical">
+          <StatisticCard.Group direction="row"  >
+            <StatisticCard
+              colSpan="20%"
+              statistic={{
+                title: '总用户',
+                value: total,
+              }}
+            />
+            <StatisticCard.Divider type="vertical" />
+            <StatisticCard
+              colSpan="35%"
+              statistic={{
+                title: '男',
+                value: manItem.value,
+                description: <StatisticCard.Statistic title="占比" value={total === 0? "100%" : ''.concat((manItem.value / total * 100).toFixed(2), '%')} />,
+              }}
+              chart={
+                <Pie
+                  height={80}
+                  width={80}
+                  innerRadius={0.7}
+                  renderer="svg"
+                  data={[{ name: '', value: total - manItem.value }, { name: '男', value: manItem.value}]}
+                  angleField="value"
+                  colorField="name"
+                  statistic={undefined}
+                  legend={false}
+                  tooltip={false}
+                  color={[ '#f5f5f5','#6394f9', ]}
+                />
+              }
+              chartPlacement="left"
+            />
+            <StatisticCard
+              colSpan="35%"
+              statistic={{
+                title: '女',
+                value: womanItem.value,
+                description: <StatisticCard.Statistic title="占比" value={total === 0? "100%" : ''.concat((womanItem.value / total * 100).toFixed(2), '%')} />,
+              }}
+              chart={
+                <Pie
+                  height={80}
+                  width={80}
+                  innerRadius={0.7}
+                  renderer="svg"
+                  data={[{ name: '女', value:  womanItem.value}, { name: '', value: total - womanItem.value }, ]}
+                  angleField="value"
+                  colorField="name"
+                  statistic={undefined}
+                  legend={false}
+                  tooltip={false}
+                  color={[ '#65daab','#f5f5f5' ]}
+                />
+              }
+              chartPlacement="left"
+            />
+          </StatisticCard.Group>
+          {/* <ProCard split="vertical">
             <StatisticCard
               statistic={{
                 title: '总人数',
@@ -93,14 +154,14 @@ const Index: React.FC<{ appId: string }> = (props) => {
               statistic={{
                 title: '性别',
                 value: dataSource
-                ? dataSource[user].genders.map(
-                    (v: UserPortraitItem) => `${v.value}${v.name}`,
-                  ).join("/")
-                : '',
+                  ? dataSource[user].genders
+                      .map((v: UserPortraitItem) => `${v.value}${v.name}`)
+                      .join('/')
+                  : '',
                 // description: <Statistic title="比例" value="8.04%" trend="down" />,
               }}
             />
-          </ProCard>
+          </ProCard> */}
         </ProCard>
         <StatisticCard
           title="设备分布"
@@ -144,7 +205,7 @@ const Index: React.FC<{ appId: string }> = (props) => {
             />
           }
         />
-        
+
         <StatisticCard
           title="年龄分布"
           chart={
