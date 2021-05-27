2b7b5aca4d54556ea311c63041bef8a5fe470626
88fantasy <88fantasy@gmail.com>
2021-05-27 14:57:29 +0800

    fix

--- config/router.config.ts
+++ config/router.config.ts
@@ -10,7 +10,7 @@ export default [
           {
             name: 'login',
             path: '/user/login',
-            component: './User/login',
+            component: './user/login',
           },
         ],
       },
